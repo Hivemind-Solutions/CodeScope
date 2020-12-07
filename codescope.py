3f02ade966a060b37f4906825c9454db2bdfd147
alexhaislip <haislipalexander@gmail.com>
2020-12-06 20:21:36 -0600

    added more functions

---
+++ codescope.py
@@ -0,0 +1,11 @@
+from codescope_functions import install_how2
+
+
+def main():
+    print("Initializing CodeScope... ")
+
+
+install_how2()
+
+if __name__ == "__main__":
+    main()
---
+++ codescope_functions.py
@@ -0,0 +1,52 @@
+from itertools import count
+import os
+
+directory = "/Users/alexanderhaislip/Desktop"
+shortcut_prefix = "~cs"
+shortcut_suffix = "."
+shortcut_Found = False
+
+
+def install_how2():
+	"""installs npm"""
+	"""checks for how2 installation or will install it if it is not found"""
+
+def install_itertools():
+	"""checks for itertools installation or will install it if it is not found"""
+
+def check():
+
+	files = os.listdir('directory')
+	print(files)
+
+while not shortcut_Found:
+
+	for file in files:
+		f = open(files, "r")
+		print(f.read())
+		if f.read() == "~cs":
+			shortcut_Found = True
+			print("Shortcut has been found!")
+		else: print("No shortcut has been found...")
+
+def translate():
+	print("Translating command...")
+	"""after identifing the shorcut identifier it will take the text in between /cs and . to 
+	convert it into how2 text in shortcut"""
+
+def query():
+	print("Finding answers...")
+	"""open new terminal session and paste how2 translation"""
+
+def copy():
+	print("Making things nice...")
+	"""copy code from terminal"""
+
+def inject():
+	print("Pooping an answer in your file")
+	"injects copied texts to end of file"
+
+def remove_shortcut():
+
+	"""removes the /cs command that was found"""
+
